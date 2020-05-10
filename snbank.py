2c488e43526832778b53510304a3bc7f71f9f3d1
Israel Adefidipe <israeladefidipe@gmail.com>
2020-05-10 23:28:55 +0100

    Update snbank.py

--- snbank.py
+++ snbank.py
@@ -20,18 +20,18 @@ while program_option == 'l':
             for i in range (1):
                 session.write('User logged in')
             session.close()
-                while True:
+            while True:
                     staff_choice =int(input('''Press 1 to create account
-                    Press 2 to Check Account Details
-                    Press 3 to Log out'''))
+Press 2 to Check Account Details
+Press 3 to Log out'''))
                     if staff_choice == 1:
                         account_name = input('Please input desired accunt name: ')
                         opening_bal = input('Your opening balance: ')
                         account_type = input('Desired account type: ')
                         account_email = input('Email: ')
-                        account_no= ''
+                        account_no= None
                         for i in range(10):
-                            account_no += random.randint(0,9)
+                            account_no = random.randint(0000000000,9999999999)
                         customer_file = open("Customer.txt", 'w+')
                         customer_file.write('Account name: %s \n' %account_name)
                         customer_file.write('Opening balance: %s \n' % opening_bal)
@@ -39,6 +39,29 @@ while program_option == 'l':
                         customer_file.write('Account email: %s \n' % account_email)
                         customer_file.close()
                         print(f'This is your account number: {account_no}')
+                        continue
+
+                    elif staff_choice == 2:
+                        while True:
+
+                                account_noVerify = int(input('Please input you account number: '))
+                                if account_noVerify == account_no:
+                                    customer_file = open('customer.txt','r')
+                                    for customer in (customer_file):
+                                        customer = customer.split('\n')
+                                        print(customer)
+                                    continue
+                                else:
+                                    print('please Try again')
+                                    continue
+
+                    if staff_choice == 3:
+                        print('Logging Out')
+                    break
+
+
+
+
 
 
 
