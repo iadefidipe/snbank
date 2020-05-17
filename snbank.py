fef21e54020cdc24f4d7b146d455ce5bc9de4ded
Israel Adefidipe <israeladefidipe@gmail.com>
2020-05-17 10:00:40 +0100

    Update snbank.py

--- snbank.py
+++ snbank.py
@@ -1,15 +1,16 @@
 import random
-program_option = input('Would you like to login: ').lower()
+program_option = input('''What can I do for you today? 
+To Login, Type 'Login'
+''').lower()
 
-print('Please login')
-while program_option == 'l':
+while program_option == 'login':
     username = input('Please Enter your Username: ')
     password = input('Please Enter your Password: ')
-    file= open("staff.txt","r")
-    Staff_file = file.readline()
+    file = open("staff.txt","r")
+    read_file = file.read()
+    Staff_file = read_file.split('\n')
     for word in Staff_file:
-        line = Staff_file.split()
-        print(Staff_file)
+        line = word.rsplit()
         username1 = (line [0])
         password1 = (line [1])
 
@@ -18,14 +19,14 @@ while program_option == 'l':
 
             session = open("session.txt","w+")
             for i in range (1):
-                session.write('User logged in')
+                session.write(f'User logged in: {username1}')
             session.close()
             while True:
                     staff_choice =int(input('''Press 1 to create account
 Press 2 to Check Account Details
 Press 3 to Log out'''))
                     if staff_choice == 1:
-                        account_name = input('Please input desired accunt name: ')
+                        account_name = input('Please input desired account name: ')
                         opening_bal = input('Your opening balance: ')
                         account_type = input('Desired account type: ')
                         account_email = input('Email: ')
@@ -50,13 +51,16 @@ Press 3 to Log out'''))
                                     for customer in (customer_file):
                                         customer = customer.split('\n')
                                         print(customer)
-                                    continue
+                                    break
+                
+
                                 else:
                                     print('please Try again')
                                     continue
 
                     if staff_choice == 3:
                         print('Logging Out')
+                        del session
                     break
 
 
@@ -69,7 +73,7 @@ Press 3 to Log out'''))
             break
             file.close()
 
+else:
+    print('Wrong details. Try again')
+    file.close()
 
-        else:
-            print('Wrong details. Try again')
-            file.close()
